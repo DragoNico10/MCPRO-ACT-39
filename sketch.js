f09e19ede2f368a50f9c41d62e9afdf81db96836
DragoNico10 <82358124+DragoNico10@users.noreply.github.com>
2022-06-21 16:43:49 -0500

    VSC

--- sketch.js
+++ sketch.js
@@ -1,11 +1,17 @@
-
+var p1, p2, ball, edges
 
 function setup(){
   createCanvas(400,400);
+  p1=createSprite(0, 200, 5, 100)
+  p2=createSprite(400, 200, 5, 100)
+  ball=createSprite(200, 200, 5, 5)
+  ball.draw = function() { ellipse(0,0,10,10) }
+  ball.velocity.x=1
+  ball.velocity.y=1
 }
 
 function draw(){
-
-  drawSprites();
+  clear()
+  drawSprites()
 }
 
