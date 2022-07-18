3b8dda35e595e7bdfbee0e211aebc0c00ff1c901
DragoNico10 <82358124+DragoNico10@users.noreply.github.com>
2022-07-18 11:58:39 -0500

    VSC

--- sketch.js
+++ sketch.js
@@ -1,16 +1,57 @@
-var p1, p2, ball, edges
+var p1, p2, ball, ballImage, p1count, p2count
+function preload(){
+  ballImage=loadImage("./assets/ball+(1).png")
+}
 
 function setup(){
   createCanvas(400,400);
-  p1=createSprite(0, 200, 5, 100)
-  p2=createSprite(400, 200, 5, 100)
+  p1=createSprite(0, 200, 15, 100)
+  p2=createSprite(400, 200, 15, 100)
   ball=createSprite(200, 200, 10, 10)
-  ball.draw = function() { ellipse(0,0,10,10) }
+  ball.setCollider("circle")
+  ball.addImage("image", ballImage)
+  ball.debug=1
+  ball.scale=0.05
+  edges=createEdgeSprites()
+  ball.velocityX=4
+  ball.velocityY=-4
+  p1count=0
+  p2count=0
 }
 
 function draw(){
   clear()
   background(255)
+  ball.bounceOff(p1)
+  ball.bounceOff(p2)
+  ball.bounceOff(edges)
+  if(p2.y<ball.y){
+    p2.y+=3
+  }
+  if(p2.y>ball.y){
+    p2.y-=3
+  }
+  if(ball.x<0){
+    ball.x=200
+    ball.velocityX=4
+    ball.velocityY=-4
+    p2count++
+  }
+  if(ball.x>400){
+    ball.x=200
+    ball.velocityX=4
+    ball.velocityY=-4
+    p1count++
+  }  
+  if(keyDown(DOWN_ARROW)){
+    p1.y+=4
+  }
+  if(keyDown(UP_ARROW)){
+    p1.y+=-4
+  }
+  textSize(20)
+  text(p1count, 150, 40)
+  text(p2count, 250, 40)
   drawSprites()
 }
 
