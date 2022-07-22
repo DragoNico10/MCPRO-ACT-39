0e54b5f413e4e6e709218f6be845c1b0d3e23640
DragoNico10 <82358124+DragoNico10@users.noreply.github.com>
2022-07-22 12:33:15 -0500

    VSC

--- sketch.js
+++ sketch.js
@@ -1,21 +1,30 @@
-var p1, p2, ball, ballImage, p1count, p2count
+var p1, p2, ball, ballImage, p1count, p2count, b1, b2
 function preload(){
-  ballImage=loadImage("./assets/ball+(1).png")
+  ballImage=loadImage("./assets/ball.png")
+  b1=loadImage("./assets/B1.png")
+  b2=loadImage("./assets/B2.png")
 }
 
 function setup(){
   createCanvas(400,400);
-  p1=createSprite(0, 200, 15, 100)
-  p2=createSprite(400, 200, 15, 100)
+  p1=createSprite(40, 200, 15, 100)
+  p2=createSprite(360, 200, 15, 100)
   ball=createSprite(200, 200, 10, 10)
   ball.setCollider("circle")
   ball.addImage("image", ballImage)
-  ball.scale=0.04
+  p1.addImage("image", b1)
+  p2.addImage("image", b2)
+  ball.scale=0.4
   edges=createEdgeSprites()
   ball.velocityX=4
   ball.velocityY=-4
   p1count=0
   p2count=0
+  p1.setCollider("rectangle", 0, -30, 70, 130)
+  p2.setCollider("rectangle", -1, -30, 60, 110)
+  for(sprite of World.allSprites){
+    sprite.debug=true
+  }
 }
 
 function draw(){
@@ -24,22 +33,22 @@ function draw(){
   ball.bounceOff(p1)
   ball.bounceOff(p2)
   ball.bounceOff(edges)
-  if(p2.y<ball.y){
+  if(p2.y-30<ball.y){
     p2.y+=3
   }
-  if(p2.y>ball.y){
+  if(p2.y-30>ball.y){
     p2.y-=3
   }
   if(ball.x<0){
     ball.x=200
     ball.velocityX=-4
-    ball.velocityY=-4
+    ball.velocityY=randYSpeed()
     p2count++
   }
   if(ball.x>400){
     ball.x=200
     ball.velocityX=4
-    ball.velocityY=-4
+    ball.velocityY=randYSpeed()
     p1count++
   }  
   if(keyDown(DOWN_ARROW)){
@@ -58,3 +67,4 @@ function draw(){
   drawSprites()
 }
 
+function randYSpeed(){let i=[-4, 4];let val=random(i);return val}
